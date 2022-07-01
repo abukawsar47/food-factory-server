3036e1fda64e3dcdbc5204e9d784d4f467d3e67d
abukawsar47 <abukawsar47ak@gmail.com>
2022-07-01 17:12:32 +0600

    get food set

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+require('./config/db')
+
+const allFoodRouter = require("./routes/foodData.route")
 
 const app = express();
 
-app.use(cors);
+app.use(cors());
 app.use(express.json())
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use('/foods', allFoodRouter)
+
+
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "./views/index.html")
+    res.sendFile(__dirname + "/./views/index.html")
 })
 
 app.use((req, res, next) => {
@@ -19,5 +26,8 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.status(500).json({ message: "500 Something Wrong" })
 })
+
+
+
 
 module.exports = app;
---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const config = require("./config");
+
+const dbURL = `mongodb+srv://${config.db.DB_USER}:${config.db.DB_PASS}@cluster0.prq70.mongodb.net/foodFactoryCollection?retryWrites=true&w=majority`
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Yay, Database Is Connected");
+    })
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    })
