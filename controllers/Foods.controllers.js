06cd8f59526d8f89367e1ca175d88734d94d04c8
khadiza21 <bibikhadiza474@gmail.com>
2022-07-02 10:36:48 +0600

    delete one and get one method added

--- controllers/Foods.controllers.js
+++ controllers/Foods.controllers.js
@@ -6,7 +6,10 @@
 4.DELETE Food By ID
 ===========================Foods API=========================
 */
+const { query } = require('express');
 const Food = require('../models/Foods.model');
+// const {ObjectId} = require('mongodb');
+const mongoose = require('mongoose');
 
 
 //1. GET all foods 
@@ -14,6 +17,18 @@ const Food = require('../models/Foods.model');
 const getAllFoods = (req, res) => {
     res.status(200).send({ message: "all foods here" })
 }
+
+const getOneFood = async (req, res) => {
+  try {
+    const food = await Food.findOne({
+      id: mongoose.Schema.ObjectId(id)
+    })
+    res.status(201).send(newFood);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
 const createNewFood = async (req, res) => {
 
   try {
@@ -32,6 +47,23 @@ const createNewFood = async (req, res) => {
 
 }
 
+const deleteFood = async (req, res) => {
+try {
+  // const id = req.params.id;
+  //  const query = {
+  //     mongoose.Schema.ObjectId: ObjectId(id)
+  //   }
+  Food.deleteOne({
+   id: mongoose.Schema.ObjectId(id)
+  })
+  res.status(200).send({ message: "all foods here" });
+  
+
+
+} catch (error) {
+  res.status(500).send(error.message);
+}
+}
 module.exports = {
-    getAllFoods, createNewFood
+    getAllFoods, createNewFood , getOneFood, deleteFood
 }
--- routes/foodData.route.js
+++ routes/foodData.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createNewFood, getAllFoods } = require('../controllers/Foods.controllers');
+const { createNewFood, getAllFoods, deleteFood, getOneFood } = require('../controllers/Foods.controllers');
 
 const router = express.Router()
 router.get('/', getAllFoods)
-
+router.get('/:id', getOneFood)
 router.post('/', createNewFood)
+router.delete('/:id', deleteFood)
 
 
 module.exports = router;
